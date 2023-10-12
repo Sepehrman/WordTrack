9a6d635fd33d1b5f2659f5c2f4e2e24bcdde34a7
Amin Jafarpour <a.jafarpour2001@gmail.com>
2023-10-11 19:13:50 -0700

    WOR-17 added pagedef folder and the files of it

---
+++ wordtrack/src/defpage/WordDef.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import SingleWordMeaning from './SingleWordMeaning'
+import Audio from './Audio';
+import Note from './Note'
+const WordDef = ({lookupWord}) => {
+  if(typeof lookupWord !== 'string' || lookupWord === '')
+  {
+    console.log(lookupWord);
+    console.log("Error: 'lookupWord' prop to 'WordDef' component is either not of type string or it is an empty string");
+    lookupWord = '';
+  }
+  // State to hold the word to look up
+  const [word, setWord] = useState(lookupWord); // Initially set to 'example', can be changed as per the requirement
+
+  useEffect(function(){setError(null); setWord(lookupWord)}, [lookupWord]);
+
+  // State to hold the fetched definition
+  const [definition, setDefinition] = useState(null);
+
+  // State to handle loading
+  const [isLoading, setIsLoading] = useState(true);
+
+  // State to handle error
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // The URL for the API endpoint, with the word inserted into the string
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+    // Fetch data from the API
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+      })
+      .then(data => {
+        setDefinition(data[0]);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        setError(error.toString());
+        setIsLoading(false);
+      });
+  }, [word]); // Re-run the effect when the 'word' state changes
+
+  let idCounter = 1;
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error occurred: {error}</p>
+      ) : (
+        <div>
+            <h1>Word: {definition.word}</h1>
+           <div>{definition.phonetics.filter(x => x.audio !== '').map(x => (<Audio key={idCounter++} audioUrlSrc={x.audio} pronunciationText={x.text}/>))}</div>
+            {definition.meanings.map(x => (<div key={idCounter++}>{<SingleWordMeaning meaningEntry={x}/>}</div>))}
+            <Note/>
+            
+          {/* <p>{JSON.stringify(definition, null, 5)}</p> */}
+ 
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default WordDef;
