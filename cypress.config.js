491cf63ed91dba7d7339be179f0b40d399f88537
jeffphan99 <jeffphan99@gmail.com>
2023-12-06 15:07:19 -0800

    changed config file

--- cypress.config.js
+++ cypress.config.js
@@ -11,11 +11,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      require('@cypress/code-coverage/task')(on, config)
-
-      return config
     },
-    baseUrl: 'http://localhost:3000',
-    experimentalRunAllSpecs: true,
+    baseUrl: 'https://word-track-h14d.vercel.app/',
   },
 });
