ec1bff69678e24e45f5cc4c0a32d26c9a957db1d
jeffphan99 <jeffphan99@gmail.com>
2023-12-06 19:34:15 -0800

    updating yaml files

--- wordtrack/cypress.config.js
+++ wordtrack/cypress.config.js
@@ -1,17 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  component: {
-    devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
+    "defaultCommandTimeout": 5000, // Set the timeout to 5000 milliseconds (5 seconds)
+  
+    "component": {
+      "devServer": {
+        "framework": "create-react-app",
+        "bundler": "webpack"
+      }
     },
-  },
+  
+    "e2e": {
+      "setupNodeEvents": function(on, config) {
+        // implement node event listeners here
+      },
+      "baseUrl": "https://word-track-seven.vercel.app"
+    }
+  });
 
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    baseUrl: 'https://word-track-seven.vercel.app',
-  },
-});
+
